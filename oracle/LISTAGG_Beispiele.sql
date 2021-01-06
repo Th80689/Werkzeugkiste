00d9ee62df9961498860274df42f361de0c6c7b9
Thomas Helmker <thomashelmker@web.de>
2021-01-06 19:59:14 +0100

    listagg ASCII code Kommentare ergänzt

--- oracle/LISTAGG_Beispiele.sql
+++ oracle/LISTAGG_Beispiele.sql
@@ -7,7 +7,15 @@ select level as bytecode
      , case when (regexp_instr(chr(level), '[!"#<=>$%&\()*+,-./:;?@^_`[~{}|]') > 0 
             or chr(level) in ('[', ']')
             or level = 39 -- '
-            ) then 'X' else NULL end SONDERZEICHEN
+            ) then 'X' else NULL 
+     end SONDERZEICHEN
+     , case
+       when level=8 then 'backspace'
+       when level=9 then 'horizontal Tab'
+       when level=10 then 'NL new line, line feed'
+       when level=13 then 'CR carriage return'
+       when level=32 then 'space'
+     end KOMMENTARE
   from dual 
  where chr(level) is not null -- Anzeige
 connect by level < 256
@@ -18,13 +26,15 @@ select distinct  output
      , BUCHSTABE
      , SONDERZEICHEN
      , WHITESPACE
+     , Kommentare
      , LISTAGG( BYTECODE
               , '|' -- Attribut [, <SEPARATOR> ]
                  ON OVERFLOW TRUNCATE '...') -- [ ON OVERFLOW TRUNCATE '<Substitug bei zu vielen Einträgen>]
    WITHIN GROUP (ORDER BY BYTECODE) -- Sortierung innerhalb der Liste
    OVER (PARTITION BY OUTPUT) as "erzeugt mit CHR(x)"  -- Gruppierungskennzeichen für Listen-Einträge
   from base
- where ZAHL is null and buchstabe is null and WHITESPACE is null and SONDERZEICHEN is null
+ where 1=1
+ --  and (ZAHL is NOT NULL OR buchstabe is NOT NULL OR WHITESPACE is NOT NULL OR SONDERZEICHEN is NOT NULL
    and length(output) > 0
  --  and bytecode between 123 and  130
 ORDER BY 6
