6368b1d2b386fdcb2ee29e22db14d270788876b8
Thomas Helmker <thomashelmker@web.de>
2021-01-24 17:23:14 +0100

    PS Parameterfile Änderung

--- powershell/PS_Script_Replace_gem-Parameterlist.ps1
+++ powershell/PS_Script_Replace_gem-Parameterlist.ps1
@@ -2,14 +2,29 @@
 # PS_Script_Replace_gem-Parameterlist
 
 # notwendige Input-Files definieren
-$inputFileParameter='paramsInput.array'
+$inputFileParameter='params.txt'
 $inputFileTemplate='Lab_Template.csv'
+$outFile='NewFile.txt'
 Set-Location ((Get-Childitem $inputFileTemplate -Recurse).DirectoryName)
 
+# Liste Parameter (Key-Value Pairs) aus File in Hashtable einlesen
 
-# Liste Parameter (Key-Value Pairs) aus File in Array einlesen
-$raw=Get-Content -Path $inputFileParameter
-$raw.Count
-$params=(Get-Content -Path $inputFileParameter -tail ($raw.Count -1))
-# Template laden und als File unter Ziel-Namen speichern
+$paramsHash=@{}
+
+foreach($item in (Import-Csv $inputFileParameter -Delimiter ";"))
+   {
+       $paramsHash.Add($($item.key),$($item.value))
+   }
+
+# Template laden und als File unter neuem Ziel-Namen speichern
+$raw=Get-Content -Path .\$inputFileTemplate -Raw | Out-File -FilePath $outFile -NoNewline
+
 # alle Keys in File suchen, durch Value ersetzen und im richtigen Format speichern 
+foreach($item in $paramsHash.PSBase.Keys)
+   {
+    $raw=Get-Content -Path .\$outFile -Raw 
+    $raw.Replace(("###" + $item + "###"), $paramsHash.($item)) | Out-File -FilePath $outFile -NoNewline
+}
+
+# Ergebnis kontrollieren
+Compare-Object -ReferenceObject (Get-Content $inputFileTemplate) -DifferenceObject (Get-Content $outFile)
